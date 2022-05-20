d48bf536e59553348b3ff5de196d35417357fa49
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-20 18:38:06 -0500

    Added FK to OrganizationId

--- OngProject/Entities/Slide.cs
+++ OngProject/Entities/Slide.cs
@@ -15,7 +15,7 @@ namespace OngProject.Entities
         [Required(ErrorMessage = "You must enter an order!")]
         public int Order { get; set; }
 
-        [Required(ErrorMessage = "You mush enter an organization id!")]
+        [Required(ErrorMessage = "You mush enter an organization id!"), ForeignKey("Organization")]
         public int OrganizationId { get; set; }
 
         public Organization Organization { get; set; }
