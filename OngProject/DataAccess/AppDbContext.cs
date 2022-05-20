84dfd8c13df4e3cb7f60bc82786d317ad29f6b39
Elieli Ferrari <elielferrari1@gmail.com>
2022-05-20 12:53:32 -0300

    added roles model

--- OngProject/DataAccess/AppDbContext.cs
+++ OngProject/DataAccess/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace OngProject.DataAccess
         {
 
         }
+        public DbSet<Roles> Roles { get; set; }
     }
 }
---
+++ OngProject/Entities/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Roles : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
