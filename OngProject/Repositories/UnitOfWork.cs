3a5c4d22c11a68a27f06678758b1b4ce0323a07e
emilhromek <emil233@live.com.ar>
2022-05-20 11:45:06 -0300

    Modified unit of work for News

--- OngProject/Repositories/UnitOfWork.cs
+++ OngProject/Repositories/UnitOfWork.cs
@@ -7,9 +7,22 @@ namespace OngProject.Repositories
     public class UnitOfWork
     {
         private readonly AppDbContext _context;
+        private readonly NewsRepository _newsRepository;
         public UnitOfWork(AppDbContext context)
         {
             _context = context;
+        }
+
+        public NewsRepository NewsRepository
+        {
+            get
+            {
+                if (_newsRepository == null)
+                {
+                    _newsRepository = new NewsRepository(_context);
+                }
+                return _newsRepository;
+            }
         }
 
         public void Save()
