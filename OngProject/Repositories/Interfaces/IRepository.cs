dd6d12d25f21a427211c20a0b52f26f18a2c04a6
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-19 21:45:33 -0500

    Entity base created.

--- OngProject/Repositories/Interfaces/IRepository.cs
+++ OngProject/Repositories/Interfaces/IRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using OngProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Repositories.Interfaces
 {
-    interface IRepository
+    public interface IRepository<T> where T : Entity
     {
+        T GetById(int id);
+        List<T> GetAll();
+
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
