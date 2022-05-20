9b68f7b17a592e15aa84cde845dd92bb713ec409
fedegrios <riosfederico@gmail.com>
2022-05-20 16:40:21 -0300

    Added News entity.

---
+++ OngProject/Entities/News.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class News : Entity
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(65535)]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+}
