d3e43e850966b629b0ab77312eb3f8fde48fe32f
NachoCeballos <nachitenz@gmail.com>
2022-05-22 18:51:45 -0300

    Testimonials model created

---
+++ OngProject/Entities/Testimonials.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Testimonials : Entity
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Image { get; set; }
+        [StringLength(65535)]
+        public string Content { get; set; }
+    }
+}
