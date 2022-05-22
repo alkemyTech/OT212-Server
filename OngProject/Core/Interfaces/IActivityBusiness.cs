69e2fa450f2ef3ae5462b8ae6180e6dac7f3015e
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-22 15:08:51 -0500

    Created IActivityBusiness and ISlideBusiness

---
+++ OngProject/Core/Interfaces/IActivityBusiness.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface IActivityBusiness
+    {
+        public IEnumerable<Activity> GetAllActivities();
+
+        public Activity GetActivity(int id);
+
+        public void InsertActivity(Activity activity);
+
+        public void UpdateActivity(int id, Activity activity);
+
+        public void DeleteActivity(int id);
+    }
+}
---
+++ OngProject/Core/Interfaces/ISlideBusiness.cs
@@ -0,0 +1,18 @@
+﻿using OngProject.Entities;
+using System.Collections.Generic;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface ISlideBusiness
+    {
+        public IEnumerable<Slide> GetAllSlides();
+
+        public Slide GetSlide(int id);
+
+        public void InsertSlide(Slide slide);
+
+        public void UpdateSlide(int id, Slide slide);
+
+        public void DeleteSlide(int id);
+    }
+}
