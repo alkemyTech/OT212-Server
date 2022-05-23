5686d039fcfd7948ccb3b1f1d0a5e54bbf418441
Elieli Ferrari <elielferrari1@gmail.com>
2022-05-23 10:41:59 -0300

    added user model

---
+++ OngProject/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Entities
+{
+    public class User : Entity
+    {
+        [Required]
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(320)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [MaxLength(255)]
+        public string Photo { get; set; }
+
+        public int RoleId { get; set; }
+
+        public Role Roles { get; set; }
+    }
+}
+
