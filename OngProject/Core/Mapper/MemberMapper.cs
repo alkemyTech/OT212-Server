8099c55ba4e8ee16bb50edd4121a2db5b46efe20
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-29 17:08:56 -0500

    Created MemberMapper and MemberDto

---
+++ OngProject/Core/Mapper/MemberMapper.cs
@@ -0,0 +1,22 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+
+namespace OngProject.Core.Mapper
+{
+    public static class MemberMapper
+    {
+        public static MemberDto MapToMemberDto(this Member member)
+        {
+            return new MemberDto
+            {
+                Name = member.Name,
+                Id = member.Id,
+                Description = member.Description,
+                FacebookUrl = member.FacebookUrl,
+                InstagramUrl = member.InstagramUrl,
+                LinedinUrl = member.LinedinUrl,
+                Image = member.Image
+            };
+        }
+    }
+}
