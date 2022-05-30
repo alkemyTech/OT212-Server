0a74bccd920f96d3098096d6d18f5d08946efc8f
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-30 12:56:36 -0500

    Fix duplicate lines

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -54,13 +54,10 @@ namespace OngProject
 
             services.AddControllers();
 
-            services.AddTransient<UnitOfWork>();
-
             services.AddTransient<IRepository<Category>, Repository<Category>>();
 
             services.AddTransient<ICategoryBusiness, CategoryBusiness>();
 
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OngProject", Version = "v1" });
