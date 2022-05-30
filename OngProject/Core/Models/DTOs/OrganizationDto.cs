b7c9ee00e39d858e476c46dcec99bb13505ca45f
fedegrios <riosfederico@gmail.com>
2022-05-30 19:20:40 -0300

    Midified organization/public to show social networks information.

--- OngProject/Core/Models/DTOs/OrganizationDto.cs
+++ OngProject/Core/Models/DTOs/OrganizationDto.cs
@@ -9,5 +9,11 @@
         public string Address { get; set; }
 
         public string Phone { get; set; }
+
+        public string FacebookUrl { get; set; }
+
+        public string LinkedinUrl { get; set; }
+
+        public string InstagramUrl { get; set; }
     }
 }
