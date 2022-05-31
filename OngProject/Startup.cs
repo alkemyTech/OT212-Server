3c397f88f79f8308219dccebbbcf25feb4044d2c
NachoCeballos <nachitenz@gmail.com>
2022-05-30 21:05:10 -0300

    Feature/US-32
    Middleware integrated in StartUp.
    Nuget package for authentication jwt added.
    Secret word for token generation added in appSettings.

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,9 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using OngProject.Core.Business;
-
+using OngProject.Core.Helper;
 using OngProject.Core.Interfaces;
 
 using OngProject.DataAccess;
@@ -19,6 +21,7 @@ using OngProject.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OngProject
@@ -38,29 +41,36 @@ namespace OngProject
 
             services.AddAppServices();
 
-            services.AddTransient<UnitOfWork>();
-
-            services.AddTransient<IRepository<Slide>, Repository<Slide>>();
-
-            services.AddTransient<ISlideBusiness, SlideBusiness>();
-
-
             services.AddDbContext<AppDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddControllers();
-
-            services.AddTransient<UnitOfWork>();
-
-            services.AddTransient<IRepository<Category>, Repository<Category>>();
-
-            services.AddTransient<ICategoryBusiness, CategoryBusiness>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OngProject", Version = "v1" });
             });
+
+            var appSettings = Configuration.GetSection("JWT").GetSection("Secret");
+            var key = Encoding.ASCII.GetBytes(appSettings.Value);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -76,6 +86,8 @@ namespace OngProject
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
