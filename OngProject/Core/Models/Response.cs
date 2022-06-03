173a1ffe0317fe4a6129b2de0f662f195dbdce37
César <cesar_delse@hotmail.com>
2022-06-02 22:15:02 -0300

    Add generic response class

---
+++ OngProject/Core/Models/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OngProject.Core.Models
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+        }
+        public Response(T data, bool succeeded = true, string[] errors = null, string message = "Success")
+        {
+            Data = data;
+            Succeeded = succeeded;
+            Errors = errors;
+            Message = message;
+        }
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+        public string Message { get; set; }
+    }
+}
