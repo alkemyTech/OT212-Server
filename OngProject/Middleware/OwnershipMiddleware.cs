ba39a78fa6b52a77b14ca0f275e1b4e18780ee0c
fedegrios <riosfederico@gmail.com>
2022-06-07 20:43:40 -0300

    Extract OwnershipMiddleware class. Refactor OwnershipMiddleware. Call OwnershipMiddleware from startup.

---
+++ OngProject/Middleware/OwnershipMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace OngProject.Middleware
+{
+    public class OwnershipMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly List<string> _authorizedRoles;
+        private readonly string _route;
+        private readonly string _method;
+
+        public OwnershipMiddleware(RequestDelegate next)
+        {
+            _next = next;
+            _authorizedRoles = new List<string> { "Administrador"};
+            _route = "/User";
+            _method = "DELETE";
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var routeProtected = context.Request.Path.StartsWithSegments(_route) && context.Request.Method == _method;
+            var canAccessToRoute = userHasAuthorizedRole(context) || compareId(context);
+
+            if (routeProtected && !canAccessToRoute)
+                    context.Response.StatusCode = 403;
+        
+            await _next.Invoke(context);
+        }
+
+        private bool compareId(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+
+            if (identity == null || !identity.Claims.Any())
+                return false;
+
+            var userId = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            var idFromParameter = context.Request.RouteValues["id"].ToString();
+
+            return userId == idFromParameter;
+        }
+
+        private bool userHasAuthorizedRole(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+
+            if (identity == null || !identity.Claims.Any())
+                return false;
+
+            var userRole = identity.Claims.FirstOrDefault(x => x.Type == identity.RoleClaimType);
+
+            return _authorizedRoles.Any(x => x == userRole.Value);
+        }
+
+    }
+
+    public static class UseOwnerwhipRestrictionsExtension
+    {
+        public static void UseOwnershipRestrictions(this IApplicationBuilder app)
+            => app.UseMiddleware<OwnershipMiddleware>();
+    }
+}
--- OngProject/Middleware/PersistActionsRestrictionsMiddleware.cs
+++ OngProject/Middleware/PersistActionsRestrictionsMiddleware.cs
@@ -12,15 +12,12 @@ namespace OngProject.Middleware
         private readonly List<string> _restrictedRestMethods;
         private readonly List<string> _authorizedRoles;
         private readonly RequestDelegate _next;
-        private readonly string _roleUser;
 
         public PersistActionsRestrictionsMiddleware(RequestDelegate next)
         {
             _next = next;
-
             _authorizedRoles = new List<string> { "Administrador" };
             _restrictedRestMethods = new List<string> { "POST", "PUT", "PATCH", "DELETE" };
-            _roleUser = "Usuario";
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -28,9 +25,6 @@ namespace OngProject.Middleware
             var isRestrictedAction = _restrictedRestMethods.Any(x => x == context.Request.Method);
             var isAuthRoute = context.Request.Path.StartsWithSegments("/Auth");
             var canAccessToRoute = isAuthRoute || !isRestrictedAction || userHasAuthorizedRole(context);
-
-            if(await Ownership(context, "/User", "DELETE"))
-                return;
             
             if (!canAccessToRoute)
                 await context.Response.WriteAsync($"You don't have authorization for this request.");
@@ -49,57 +43,6 @@ namespace OngProject.Middleware
             var userRole = identity.Claims.FirstOrDefault(x => x.Type == identity.RoleClaimType);
             
             return _authorizedRoles.Any(x => x == userRole.Value);
-        }
-
-        private async Task<bool> Ownership(HttpContext context, string route, string method)
-        {
-            var RouteProtected = context.Request.Path.StartsWithSegments(route) && context.Request.Method == method;
-
-            if (RouteProtected)
-            {
-                var canAccessToRoute = userHasAuthorizedRole(context) || userHasRole(context) && compareId(context);
-
-                if (canAccessToRoute)
-                {
-                    await _next.Invoke(context);
-                    return true;
-                }
-                if (!canAccessToRoute)
-                {
-                    context.Response.StatusCode = 403;
-                    return true;
-                }
-            }
-            return false;
-        }
-        private string getUserId(HttpContext context)
-        {
-            var identity = context.User.Identity as ClaimsIdentity;
-            //var userId = context.User.Claims.ToArray()[2].Value;
-            var userId = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
-            return userId;
-        }
-
-        private bool compareId(HttpContext context)
-        {
-            var idParameter = context.Request.RouteValues["id"].ToString();
-            if (getUserId(context) == idParameter)
-                return true;
-            else return false;
-        }
-
-        private bool userHasRole(HttpContext context)
-        {
-            var identity = context.User.Identity as ClaimsIdentity;
-
-            if (identity == null || !identity.Claims.Any())
-                return false;
-
-            var userRole = identity.Claims.FirstOrDefault(x => x.Type == identity.RoleClaimType);
-
-            if (userRole.Value == _roleUser)
-                return true;
-            return false;
         }
     }
 
