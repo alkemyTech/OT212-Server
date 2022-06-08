537e3358fbd9abbd7dbae30fff06a25b69ac80ec
fedegrios <riosfederico@gmail.com>
2022-06-08 03:32:11 -0300

    Refactor PersistActionsRestrictionsMiddleware to allow exceptions.

--- OngProject/Middleware/PersistActionsRestrictionsMiddleware.cs
+++ OngProject/Middleware/PersistActionsRestrictionsMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -10,39 +11,76 @@ namespace OngProject.Middleware
     public class PersistActionsRestrictionsMiddleware
     {
         private readonly List<string> _restrictedRestMethods;
-        private readonly List<string> _authorizedRoles;
+        private List<Permission> permissions;
         private readonly RequestDelegate _next;
 
         public PersistActionsRestrictionsMiddleware(RequestDelegate next)
         {
             _next = next;
-            _authorizedRoles = new List<string> { "Administrador" };
             _restrictedRestMethods = new List<string> { "POST", "PUT", "PATCH", "DELETE" };
+            permissions = new List<Permission>();
+
+            ConfigurePermissions();
+        }
+
+        private void ConfigurePermissions() 
+        {
+            permissions.Add(new Permission { Role = "Administrador"});
+            permissions.Add(new Permission { Route = "/Auth"});
+            permissions.Add(new Permission { Route = "/User", Method = "DELETE"});
+
         }
 
         public async Task InvokeAsync(HttpContext context)
-        { 
+        {
             var isRestrictedAction = _restrictedRestMethods.Any(x => x == context.Request.Method);
-            var isAuthRoute = context.Request.Path.StartsWithSegments("/Auth");
-            var canAccessToRoute = isAuthRoute || !isRestrictedAction || userHasAuthorizedRole(context);
-            
+            var canAccessToRoute = !isRestrictedAction || HasPermissions(context);
+
             if (!canAccessToRoute)
-                await context.Response.WriteAsync($"You don't have authorization for this request.");
+                context.Response.StatusCode = 403; 
 
             else
                 await _next.Invoke(context);
         }
 
-        private bool userHasAuthorizedRole(HttpContext context)
+        private bool HasPermissions(HttpContext context)
         {
+            var lstPermission = new List<Permission>();
+            var route = context.Request.Path;
+            var method = context.Request.Method;
+            var role = "";
+
             var identity = context.User.Identity as ClaimsIdentity;
 
-            if (identity == null || !identity.Claims.Any())
-                return false;
+            if (identity != null && identity.Claims.Any())
+                role = identity.Claims.FirstOrDefault(x => x.Type == identity.RoleClaimType).Value;
 
-            var userRole = identity.Claims.FirstOrDefault(x => x.Type == identity.RoleClaimType);
-            
-            return _authorizedRoles.Any(x => x == userRole.Value);
+
+            if (!string.IsNullOrEmpty(role))
+            {
+                if (permissions.Any(p => p.Role == role && string.IsNullOrEmpty(p.Route)))
+                    return true;
+
+                lstPermission = permissions.Where(p => p.Role == role && route.StartsWithSegments(p.Route)).ToList();
+
+                if (lstPermission.Any(p => p.Method == method)
+                    || lstPermission.Any(p => string.IsNullOrEmpty(p.Method)))
+                    return true;
+            }
+
+            lstPermission = permissions.Where(p => !string.IsNullOrEmpty(p.Route) && route.StartsWithSegments(p.Route)).ToList();
+
+            return lstPermission.Any(p => p.Method == method)
+                || lstPermission.Any(p => string.IsNullOrEmpty(p.Method));
+        }
+
+        internal class Permission
+        {
+            public string Role { get; set; }
+
+            public string Route { get; set; }
+
+            public string Method { get; set; }
         }
     }
 
