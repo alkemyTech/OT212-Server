8e56c1088ffc7fcb3185c93beda36cd1adc08148
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-06-13 23:44:41 -0500

    Created documentation in UserController

--- OngProject/Core/Models/DTOs/UserEditDto.cs
+++ OngProject/Core/Models/DTOs/UserEditDto.cs
@@ -5,19 +5,31 @@ namespace OngProject.Core.Models.DTOs
 {
     public class UserEditDto
     {
+        /// <summary>
+        /// Primer nombre del usuario a editar.
+        /// </summary>
         [Required]
         [MaxLength(255)]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Apellido del usuario a editar.
+        /// </summary>
         [Required]
         [MaxLength(255)]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Correo electrónico del usuario a editar.
+        /// </summary>
         [Required]
         [EmailAddress]
         [MaxLength(320)]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Imagen del usuario a editar.
+        /// </summary>
         public IFormFile Photo { get; set; }
 
     }
