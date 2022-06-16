30ae026a4b7658714c8c755b1d2902556da8a1c7
CesarDelseggio <cesar_delse@hotmail.com>
2022-06-16 16:50:20 -0300

    Update MembersControllerTests.cs

--- OngProjectTests/Controllers/MembersControllerTests.cs
+++ OngProjectTests/Controllers/MembersControllerTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OngProjectTests.Controllers.Tests
+namespace OngProject.Controllers.Tests
 {
     [TestClass()]
     public class MembersControllerTests
