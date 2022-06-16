cd44da35411f15e1ad8c448be8612332154954d3
César <cesar_delse@hotmail.com>
2022-06-16 01:57:16 -0300

    Add test to get all users controllers

---
+++ OngProjectTests/Controllers/BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OngProject.Core.Business;
+using OngProject.Core.Interfaces;
+using OngProject.DataAccess;
+using OngProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProjectTests.Controllers
+{
+    public class BaseTest
+    {
+        protected AppDbContext GetDbContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(dbName).Options;
+            var dbContext = new AppDbContext(options);
+
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+
+        protected UnitOfWork GetUnitOfWork()
+        {
+            var unitOfWork = new UnitOfWork(GetDbContext(Guid.NewGuid().ToString()));
+
+            return unitOfWork;
+        }
+
+        protected IUserBusiness GetUserBusiness()
+        {
+            var userBusiness = new UserBusiness(GetUnitOfWork());
+
+            return userBusiness;
+        }
+    }
+}
---
+++ OngProjectTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OngProject.Controllers;
+using OngProject.Core.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OngProjectTests.Controllers.Tests
+{
+    [TestClass()]
+    public class UsersControllerTests : BaseTest
+    {
+        [TestMethod()]
+        public async Task GetUserTest()
+        {
+            //Arrange
+            var userServices = GetUserBusiness();
+            var list = await userServices.GetAll();
+
+            var Controller = new UserController(userServices);
+            Controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            //Act
+            var resp = await Controller.GetAll();
+            var result = ((resp.Result) as OkObjectResult)?.Value as List<UserDto>;
+
+            //Assert
+            Assert.AreEqual(list?.Count(), result?.Count());
+        }
+    }
+}
