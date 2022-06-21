6c207afa154113b9e5347c695e8c1c696842c295
fedegrios <riosfederico@gmail.com>
2022-06-20 23:11:25 -0300

    Feature/Us-107 Created test for OrganizationController Get method. Created test for OrganizationController Update method.

---
+++ OngProjectTests/Mocks/OrganizationBusinessMock.cs
@@ -0,0 +1,45 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.Core.Models;
+using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngProjectTests.Mocks
+{
+    public class OrganizationBusinessMock : IOrganizationBusiness
+    {
+        public Response<OrganizationDetailsDto> GetResponse { get; set; }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<OrganizationDetailsDto>> Get()
+            => await Task.FromResult(GetResponse);
+
+        public Task<List<Organization>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Organization> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(Organization entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrganizationDto> Update(int id, OrganizationUpdateDto organizationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
