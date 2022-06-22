3827c01fc675414b3973e0ce19b4c8db111a360f
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-06-21 19:24:52 -0500

    Created testing in Contacts Controller

--- OngProjectTests/Controllers/BaseTest.cs
+++ OngProjectTests/Controllers/BaseTest.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using OngProject;
 using OngProject.Core.Business;
 using OngProject.Core.Interfaces;
+using OngProject.Core.Models.DTOs;
 using OngProject.DataAccess;
 using OngProject.Repositories;
 using System;
@@ -13,6 +19,26 @@ namespace OngProjectTests.Controllers
 {
     public class BaseTest
     {
+        private IConfiguration? _config;
+
+        protected IConfiguration Config
+        {
+            get
+            {
+                if (_config == null)
+                {
+                    var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", optional: false);
+                    _config = builder.Build();
+                }
+
+                return _config;
+            }
+        }
+        public BaseTest()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(Config);
+        }
         protected AppDbContext GetDbContext(string dbName)
         {
             var options = new DbContextOptionsBuilder<AppDbContext>()
@@ -36,6 +62,56 @@ namespace OngProjectTests.Controllers
             var userBusiness = new UserBusiness(GetUnitOfWork());
 
             return userBusiness;
+        }
+
+        protected IContactBusiness GetContactBusiness()
+        {
+            return new ContactBusiness(GetUnitOfWork(), new EmailServices(Config));
+        }
+
+        protected static HttpClient GetHttpClient()
+        {
+            var application = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(webHostBuilder => 
+                { 
+                    webHostBuilder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.SingleOrDefault(
+                                        d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                        services.Remove(descriptor!);
+
+                        services.AddDbContext<AppDbContext>(options =>
+                        {
+                            options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                        }, ServiceLifetime.Singleton);
+                    });
+                });
+
+            var DbContext = application.Services.CreateScope()
+                                       .ServiceProvider.GetService<AppDbContext>();
+
+            DbContext?.Database.EnsureCreated();
+
+            return application.CreateClient();
+        }
+
+        protected async Task HttpClientLogin(HttpClient httpClient, string email, string password)
+        {
+            string token = string.Empty;
+
+            var user = new LoginDto { Email = email, Password = password};
+
+            var json = JsonConvert.SerializeObject(user);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var resp = await httpClient.PostAsync("auth/login", content);
+            if(resp.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                token = await resp.Content.ReadAsStringAsync();
+            }
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
         }
     }
 }
